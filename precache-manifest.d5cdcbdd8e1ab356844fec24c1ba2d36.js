404ac4e8252a1598a6855cfccbf2d1000e47d335
Andrzej Korbiel <akorbiel@online.saba.com>
2020-03-13 11:09:07 +0100

    Updates

---
+++ precache-manifest.d5cdcbdd8e1ab356844fec24c1ba2d36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb4257ee8b3df2480bca9f1aee0c94a",
+    "url": "/simple-blogging-app/index.html"
+  },
+  {
+    "revision": "1c6d74761f18b7da7b6f",
+    "url": "/simple-blogging-app/static/css/2.0cee8fd6.chunk.css"
+  },
+  {
+    "revision": "da7c2063120fcc356593",
+    "url": "/simple-blogging-app/static/css/main.e467e554.chunk.css"
+  },
+  {
+    "revision": "1c6d74761f18b7da7b6f",
+    "url": "/simple-blogging-app/static/js/2.4e113b95.chunk.js"
+  },
+  {
+    "revision": "3d72a9594b3c4ca43da4afa8e1d7849a",
+    "url": "/simple-blogging-app/static/js/2.4e113b95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da7c2063120fcc356593",
+    "url": "/simple-blogging-app/static/js/main.cf95c1d4.chunk.js"
+  },
+  {
+    "revision": "988e411f933931d2cf38",
+    "url": "/simple-blogging-app/static/js/runtime-main.901539aa.js"
+  }
+]);
